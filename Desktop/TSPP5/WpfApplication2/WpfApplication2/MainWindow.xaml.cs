3234d1c3ecf3f63f4f4157aad27547772c32df55
hollyandy <andyredator@yandex.ru>
2016-12-07 12:23:26 +0200

    first commit

---
+++ Desktop/TSPP5/WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int i=0,k=0,v=0,z=0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+ 
+
+        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.ToString().Equals("* * * *") && textBox.IsFocused == true)
+            {
+                textBox.Text = "";
+            }
+
+           
+
+
+        }
+
+        private void textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.ToString().Equals("") && textBox.IsFocused == false)
+            {
+                textBox.Text = "* * * *";
+            }
+
+        }
+
+        private void button1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        
+        }
+
+        private void button1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Hidden;
+            textBox1.Text = passwordBox.Password;
+            textBox1.Visibility = Visibility.Visible;
+        }
+
+        private void button1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBox1.Visibility = Visibility.Hidden;
+            textBox1.Text= "";
+            passwordBox.Visibility = Visibility.Visible;
+        }
+
+        private void Border1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (i == 0)
+            {
+                SolidColorBrush b = new SolidColorBrush();
+               b.Color = Color.FromArgb(255, 243, 240, 240);
+                i++;
+            }
+            else { 
+            SolidColorBrush a = new SolidColorBrush();
+            a.Color = Color.FromArgb(255, 255, 255, 161);
+       
+            }
+            //#FFF3F0F0 207,207,207
+            //#FFCFCFCF 243,240,240
+        }
+
+        private void Border1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush a = new SolidColorBrush();
+            a.Color = Color.FromArgb(255, 243, 240, 240);
+
+        }
+
+
+        private void textBox_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+          
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (z >= 1)
+            {
+                if (textBox.Text.ToString().Equals("* * * *"))
+                {
+                    image1.Visibility = Visibility.Hidden;
+                    label1.Content = "";
+                }
+                else
+                {
+
+                    if (textBox.Text.ToString().Length < 4)
+                    {
+                        image1.Visibility = Visibility.Visible;
+                        label1.Content = "Перевірте введений UI";
+                    }
+
+                    if (textBox.Text.ToString().Length == 4)
+                    {
+                        image1.Visibility = Visibility.Hidden;
+
+                        label1.Content = "";
+                    }
+                }
+            }
+            else
+            {
+               z++;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.ToString().Equals("1111"))
+            {
+                MessageBox.Show("Ви успішно увійшли. Вітаємо!");
+            }
+            else
+            { label1.Content = "Перевірте правильність введених даних"; }
+        }
+
+        private void textBox_Copy_TextChanged(object sender, TextChangedEventArgs e)
+        { 
+            if (v >= 1)
+            {
+                if (textBox.Text.ToString().Equals("* * * *"))
+                {
+                    image1.Visibility = Visibility.Hidden;
+                    label1.Content = "";
+                }
+                else
+                {
+
+                    if (textBox.Text.ToString().Length < 4)
+                    {
+                        image1.Visibility = Visibility.Visible;
+                        label1.Content = "Перевірте введений UI";
+                    }
+
+                    if (textBox.Text.ToString().Length == 4)
+                    {
+                        image1.Visibility = Visibility.Hidden;
+                        label1.Content = "";
+
+                    }
+                }
+            }
+            else
+            {
+                v++;
+            }
+        }
+
+        private void textBox_Copy1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (k >= 1)
+            {
+                if (textBox.Text.ToString().Equals("* * * *"))
+                {
+                    image1.Visibility = Visibility.Hidden;
+                    label1.Content = "";
+                }
+                else {
+
+                if (textBox.Text.ToString().Length< 4)
+                {
+                    image1.Visibility = Visibility.Visible;
+                        label1.Content = "Перевірте введений UI";
+                    }
+
+                if (textBox.Text.ToString().Length == 4)
+                { 
+
+                    image1.Visibility = Visibility.Hidden;
+                        label1.Content = "";
+                    }
+                }
+            }
+            else
+            {
+                k++;
+            }
+
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+         
+        }
+    }
+}
---
+++ Desktop/TSPP5/WpfApplication2/WpfApplication2/Window5.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 a = new Window1();
+            a.Visibility = Visibility.Visible;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 a = new Window2();
+            a.Visibility = Visibility.Visible;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 a = new Window3();
+            a.Visibility = Visibility.Visible;
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Window4 a = new Window4();
+            a.Visibility = Visibility.Visible;
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow a = new MainWindow();
+            a.Visibility = Visibility.Visible;
+        }
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            Window6 a = new Window6();
+            a.Visibility = Visibility.Visible;
+        }
+    }
+}
